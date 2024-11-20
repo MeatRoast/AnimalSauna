b16073fe4562d85c19c3b4cadea91a261728ff46
Kim Dohwan <57842896+MeatRoast@users.noreply.github.com>
2024-11-21 00:21:28 +0900

    업데이트

--- system/bot.py
+++ system/bot.py
@@ -1,36 +1,50 @@
 # 라이브러리
 import discord
 from discord.ext import commands, tasks
-from datetime import datetime, timedelta
+from datetime import datetime
 import atexit
 from dotenv import dotenv_values
-
-#Bot File
+import asyncio
 from system.DB import DB
 from system.status import *
 
-env_variables = dotenv_values("./system/private/.env") # env파일 봇 TOKEN을 가져옴
+env_variables = dotenv_values("./system/private/.env")  # env파일에서 BOT TOKEN 가져옴
+if "BOT_TOKEN" not in env_variables:
+    print("Error: BOT_TOKEN is missing in .env file")
+    exit(1)
+
 intents = discord.Intents.all()
 bot = commands.Bot(intents=intents)
 
+# Commands Load
+try:
+    bot.load_extension("commands")
+    bot.load_extension("event")
+except Exception as e:
+    print(f"Error loading extensions: {type(e).__name__} - {e}")
 
-bot.load_extension("commands") # 커멘드 정보 가져옴
+@bot.event
+async def on_connect():
+    print("Connecting to the Discord Server!")
 
 @bot.event
 async def on_ready():
-    await setup_status(bot)
-    # await DB.connect()
-    print('')
-    print('')
-    print("             Copyright ©2021 MeatRoast Inc. All rights reserve")
-    print("             ")
-    print('')
-    print("         BOT NAME:", bot.user.name)
-    print("         BOT Clint ID:", bot.user.id)
-    print('')
-    print("------------------")
+    try:
+        await setup_status(bot)
+        await DB.connect()
+        print("\n\n")
+        print("             Copyright ©2021 MeatRoast Inc. All rights reserved")
+        print(f"         BOT NAME: {bot.user.name}")
+        print(f"         BOT Client ID: {bot.user.id}")
+        print("\n------------------")
+    except Exception as e:
+        print(f"[{datetime.now()}] Login Error: {type(e).__name__} - {e}")
 
-#봇 서비스 실행
+@bot.event
+async def on_disconnect():
+    print("Disconnected from Discord server! Waiting for automatic reconnection...")
+
+# 봇 서비스 실행
 def run_bot():
     global chat_log_file  # 전역 변수로 설정
     chat_log_file = open(f"./chatlog/{datetime.now().strftime('%Y-%m-%d')}.txt", "a", encoding="utf-8")
